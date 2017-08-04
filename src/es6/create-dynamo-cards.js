5f95945b44292ebf8c019ff0227fccea46419803
Chris Reeve <paradigmsareconstructed@gmail.com>
2017-08-04 09:33:00 -0700

    Added prompt and am now able to clear out the entire collection of cards

--- src/es6/create-dynamo-cards.js
+++ src/es6/create-dynamo-cards.js
@@ -1,4 +1,6 @@
 import Cards from './libs/cards';
+import prompt from 'prompt';
+import colors from 'colors/safe';
 
 let existingCardCollection = [],
 	cardsAPI;
@@ -9,8 +11,32 @@ async function clearCollection() {
 	console.log('Fetching the existing collection of cards ...');
 	existingCardCollection = await cardsAPI.getControversySlugs();
 
-	console.log('\nThe current set of slugs is:')
+	console.log('\nThe current set of cards is:')
 	console.log(existingCardCollection);
+
+	const schema = {
+		properties: {
+			confirmation: {
+				pattern: /^(y|n|yes|no)$/,
+				description: colors.red.bold('Are you sure you want to delete the existing collection of controversy cards?'),
+				message: colors.white('(y)es or (n)o'),
+				required: true
+			}
+		}
+	};
+
+	prompt.start();
+
+	prompt.get(schema, (err, result) => {
+		if (result.confirmation === 'y' ||
+			result.confirmation === 'yes') {
+
+			existingCardCollection.forEach(slug => {
+				console.log('\nDeleting controversy card ' + slug);
+				cardsAPI.deleteControversy(slug);
+			});
+		}
+	});
 }
 
 try {
--- src/js/create-dynamo-cards.js
+++ src/js/create-dynamo-cards.js
@@ -4,6 +4,14 @@ var _cards = require('./libs/cards');
 
 var _cards2 = _interopRequireDefault(_cards);
 
+var _prompt = require('prompt');
+
+var _prompt2 = _interopRequireDefault(_prompt);
+
+var _safe = require('colors/safe');
+
+var _safe2 = _interopRequireDefault(_safe);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var existingCardCollection = [],
@@ -15,8 +23,31 @@ async function clearCollection() {
 	console.log('Fetching the existing collection of cards ...');
 	existingCardCollection = await cardsAPI.getControversySlugs();
 
-	console.log('\nThe current set of slugs is:');
+	console.log('\nThe current set of cards is:');
 	console.log(existingCardCollection);
+
+	var schema = {
+		properties: {
+			confirmation: {
+				pattern: /^(y|n|yes|no)$/,
+				description: _safe2.default.red.bold('Are you sure you want to delete the existing collection of controversy cards?'),
+				message: _safe2.default.white('(y)es or (n)o'),
+				required: true
+			}
+		}
+	};
+
+	_prompt2.default.start();
+
+	_prompt2.default.get(schema, function (err, result) {
+		if (result.confirmation === 'y' || result.confirmation === 'yes') {
+
+			existingCardCollection.forEach(function (slug) {
+				console.log('\nDeleting controversy card ' + slug);
+				cardsAPI.deleteControversy(slug);
+			});
+		}
+	});
 }
 
 try {
